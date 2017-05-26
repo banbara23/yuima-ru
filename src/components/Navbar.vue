d7a2515ecdc2040a715e93d95c18b9fdf5aaf361
池村和剛 <z1tg6774@h1408327.local>
2017-05-26 10:03:53 +0900

    ナビ表示名を正しく修正

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,9 +4,9 @@
       <a href="/"
          class="brand-logo center">ゆいま〜る</a>
       <ul class="left hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li class="active"><a href="collapsible.html">JavaScript</a></li>
+        <li><a href="/schedule">出席登録</a></li>
+        <li><a href="/member">メンバー</a></li>
+        <li><a href="/buhi">部費</a></li>
       </ul>
 
     </div>
