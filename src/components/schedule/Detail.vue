84d81b5c5d0f2dac4f4784590243183359c0ff54
池村和剛 <z1tg6774@h1408327.local>
2017-05-31 15:04:30 +0900

    スケジュール詳細をリニューアル

--- src/components/schedule/Detail.vue
+++ src/components/schedule/Detail.vue
@@ -4,58 +4,36 @@
     <p>日時：2017/05/20 10〜14時</p>
     <p>場所：練馬春日小学校</p>
     <p>コメント：今回はたくさんくるぞー</p>
-  
-    <div class="card">
-      <div class="card-content">
-        <table class="bordered">
-          <thead>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>回答</th>
-              <th>コメント</th>
-              <th></th>
-            </tr>
-          </thead>
-  
-          <tbody>
-            <tr>
-              <td>
-                <img src="http://www.gemani.org/img/_1397177820.12109_843.png" alt="" class="circle">
-              </td>
-              <td>Alvin</td>
-              <td>出席</td>
-              <td>いくます</td>
-              <td class="right">
-                <a class="waves-effect waves-light btn" href="/schedule/entry/1">登録</a>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img src="http://www.gemani.org/img/_1397177820.12109_843.png" alt="" class="circle">
-              </td>
-              <td>Alan</td>
-              <td>-</td>
-              <td></td>
-              <td class="right">
-                <a class="waves-effect waves-light btn" href="/schedule/entry/1">登録</a>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img src="http://www.gemani.org/img/_1397177820.12109_843.png" alt="" class="circle">
-              </td>
-              <td>Jonathan</td>
-              <td>欠席</td>
-              <td>予定がありキャンセルします...</td>
-              <td class="right">
-                <a class="waves-effect waves-light btn" href="/schedule/entry/1">登録</a>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
+
+    <ul class="collection">
+      
+    <li class="collection-item avatar">
+      <img src="../../assets/alvin.png" alt="" class="circle">
+      <span class="title">名前</span>
+      <p>コメント</p>
+      <div class="secondary-content"><a class="blue waves-effect waves-light btn" href="/schedule/entry/1">参加</a></div>
+    </li>
+
+    <li class="collection-item avatar">
+      <i class="material-icons circle ">perm_identity</i>
+      <span class="title">名前</span>
+      <p>コメント</p>
+      <div class="secondary-content"><a class="orange waves-effect waves-light btn" href="/schedule/entry/1">未定</a></div>
+    </li>
+    <li class="collection-item avatar">
+      <i class="material-icons circle">perm_identity</i>
+      <span class="title">名前</span>
+      <p>コメント</p>
+      <div class="secondary-content"><a class="red waves-effect waves-light btn" href="/schedule/entry/1">欠席</a></div>
+    </li>
+    <li class="collection-item avatar">
+      <i class="material-icons circle">perm_identity</i>
+      <span class="title">名前</span>
+      <p>コメント</p>
+      <div class="secondary-content"><a class="red waves-effect waves-light btn" href="/schedule/entry/1">欠席</a></div>
+    </li>
+  </ul>
+
    <!--登録ボタン-->
         <button @click="goScheduleTop" class="btn waves-effect waves-light" type="submit" name="action">
                 スケジュールトップ
