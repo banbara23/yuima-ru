b77e0edcd9ba9f1a78e363b0db99a34c1c48e506
ikemura <8417910+banbara23@users.noreply.github.com>
2017-08-07 22:53:32 +0900

    予定詳細に選択した予定とメンバー一覧が表示された

--- src/components/schedule/Detail.vue
+++ src/components/schedule/Detail.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="detail">
+    <!-- {{detail}} {{member}}  -->
+    {{val}}
     <vue-progress-bar></vue-progress-bar>
     <!--情報-->
-    <h2>予定詳細</h2>
-    <div class="row card">
+    <h3>予定詳細</h3>
+    <div class="row card" v-if="detail">
       <div class="card-content">
         <span class="card-title">{{detail.title}}</span>
         <p>日時：{{detail.date}} {{detail.startTime}}〜{{detail.endTime}}</p>
         <p>場所：{{detail.place}}</p>
         <p>コメント：{{detail.comment}}</p>
         <!--<button @click="goEdit"
-                    class="btn waves-effect waves-light"
-                    type="submit"
-                    name="action">
-              編集
-            </button>-->
+                          class="btn waves-effect waves-light"
+                          type="submit"
+                          name="action">
+                    編集
+                  </button>-->
       </div>
       <div class="card-action">
         <router-link to="/schedule/New">修正</router-link>
@@ -22,7 +24,7 @@
     </div>
   
     <!--まとめ数字-->
-    <div class="row section card">
+    <!-- <div class="row section card">
       <div class="col s4 l4 center">
         <h4>参加</h4>
         <h4>1</h4>
@@ -35,34 +37,28 @@
         <h4>未定</h4>
         <h4>1</h4>
       </div>
-    </div>
+    </div> -->
   
     <!--リスト-->
     <div class="row">
-      <ul class="collection card">
-        <li class="collection-item avatar"
-            v-for="item in anArray"
-            v-bind:key="item.id">
+      <ul class="collection card" v-if="members">
+        <li class="collection-item avatar" v-for="member in members" v-bind:key="member['key']">
           <!--<img v-if="item.image"
-                       :src="item.image"
-                       alt=""
-                       class="circle">-->
+                             :src="item.image"
+                             alt=""
+                             class="circle">-->
           <i class="material-icons circle ">perm_identity</i>
-          <span class="title">{{item.name}}</span>
-          <p>{{item.comment}}</p>
+          <span class="title">{{member.name}}</span>
+          <p>{{member.comment}}</p>
           <div class="secondary-content">
-            <router-link class="blue waves-effect waves-light btn"
-                         to="/schedule/entry/1">参加</router-link>
+            <router-link class="blue waves-effect waves-light btn" to="/schedule/entry/1">参加</router-link>
           </div>
         </li>
       </ul>
     </div>
   
     <!--登録ボタン-->
-    <button @click="goScheduleTop"
-            class="btn waves-effect waves-light"
-            type="submit"
-            name="action">
+    <button @click="goScheduleTop" class="btn waves-effect waves-light" type="submit" name="action">
       スケジュールトップ
     </button>
   </div>
@@ -71,90 +67,49 @@
 <script>
 import firebase from 'firebase'
 const db = firebase.database()
-// import TopMenu from '@/components/TopMenu'
+
 export default {
   name: 'name',
   data() {
     return {
       title: 'スケジュール詳細',
-      anArray: []
-    }
-  },
-  firebase() {
-    return {
-      detail: {
-        source: db.ref("/schedule/")
-          .child(this.$route.params.id),
-        asObject: true,
-        readyCallback: function () {
-          this.$Progress.finish();
-        }
-      }
-    }
-  },
-  firebase: {
-    anArray: {
-      source: db.ref("/member"),
-      readyCallback: function () {
-        this.$Progress.finish()
-      }
+      detail: {},
+      members: [],
+      val: null
     }
   },
   created() {
     this.$Progress.start();
-    this.anArray = [
-      {
-        id: 'moja',
-        name: '名前',
-        comment: '何かしらのコメント',
-        answer: '回答',
-        image: '../../assets/alvin.png'
+  },
+  firebase() {
+    return {
+      members: {
+        source: db.ref("/member"),
+        readyCallback: function (data) {
+          if (detail.schedule != undefined) {
+            
+            // 回答済みの予定を組み立てる処理
+          }
+          this.val = data
+          this.$Progress.finish()
+        }
       },
-      {
-        id: '2',
-        name: '名前',
-        comment: '何かしらのコメント',
-        answer: '回答',
-        image: '../../assets/alvin.png'
-      },
-      {
-        id: '3',
-        name: '名前',
-        comment: '何かしらのコメント',
-        answer: '回答',
-        image: '../../assets/alvin.png'
-      },
-      {
-        id: '4',
-        name: '名前',
-        comment: '何かしらのコメント',
-        answer: '回答',
-        image: '../../assets/alvin.png'
-      },
-      {
-        id: '5',
-        name: '名前',
-        comment: '何かしらのコメント',
-        answer: '回答',
-        image: '../../assets/alvin.png'
-      },
-      {
-        id: '6',
-        name: '名前',
-        comment: '何かしらのコメント',
-        answer: '回答',
-        image: '../../assets/alvin.png'
-      },
-      {
-        id: '7',
-        name: '名前',
-        comment: '何かしらのコメント',
-        answer: '回答',
-        image: '../../assets/alvin.png'
+      detail: {
+        source: db.ref("schedule").child(this.$route.params.id),
+        asObject: true,
+        readyCallback: function () {
+          this.$Progress.finish()
+        }
       }
-    ]
+    }
+  },
+  computed: {
+   test: function() {
+      console.log("call!!!")
+    }
   },
   methods: {
+ 
     goScheduleTop: function () {
       this.$router.push('/schedule');
     },
