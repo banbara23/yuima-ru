09e36ba064d58bacec6e99fe9026266c60667de4
ikemura <8417910+banbara23@users.noreply.github.com>
2017-08-22 15:07:45 +0900

    サイドナビ修正、幅200で押下で閉じる

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,16 +2,33 @@
   <nav>
     <div class="nav-wrapper">
       <router-link to="/" class="brand-logo center">ゆいま〜る</router-link>
-      <router-link to="#" data-activates="mobile-nav" class="button-collapse"><i class="material-icons">menu</i></router-link>
+      <router-link to="#" data-activates="mobile-nav" class="button-collapse">
+        <i class="material-icons">menu</i>
+      </router-link>
       <ul class="right hide-on-med-and-down">
-        <li><router-link to="/schedule">出席登録</router-link></li>
-        <li><router-link to="/member">メンバー</router-link></li>
-        <li><router-link to="/buhi">部費</router-link></li>
+        <li>
+          <router-link to="/schedule">出席登録</router-link>
+        </li>
+        <li>
+          <router-link to="/member">メンバー</router-link>
+        </li>
+        <li>
+          <router-link to="/buhi">部費</router-link>
+        </li>
       </ul>
       <ul class="side-nav" id="mobile-nav">
-        <li><router-link to="/schedule">出席登録</router-link></li>
-        <li><router-link to="/member">メンバー</router-link></li>
-        <li><router-link to="/buhi">部費</router-link></li>
+        <li>
+          <router-link to="/">トップ</router-link>
+        </li>
+        <li>
+          <router-link to="/schedule">出席登録</router-link>
+        </li>
+        <li>
+          <router-link to="/member">メンバー</router-link>
+        </li>
+        <li>
+          <router-link to="/buhi">部費</router-link>
+        </li>
       </ul>
     </div>
   </nav>
@@ -19,7 +36,10 @@
 <script>
 export default {
   mounted() {
-    $(".button-collapse").sideNav();
+    $(".button-collapse").sideNav({
+      menuWidth: 200,
+      closeOnClick: true
+    });
   }
 }
 </script>
