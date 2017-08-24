ded908030c96548323f3642440e21fd19e325e8f
ikemura <8417910+banbara23@users.noreply.github.com>
2017-08-24 16:51:10 +0900

    回答を実装、まとめ数字はローカルで計算して表示

--- src/components/schedule/Detail.vue
+++ src/components/schedule/Detail.vue
@@ -2,89 +2,88 @@
   <div class="detail">
     <vue-progress-bar></vue-progress-bar>
     <h3>予定詳細</h3>
-    <div class="row card" v-if="detail">
+    <div class="row card" v-if="schedule">
       <div class="card-content">
-        <span class="card-title">{{detail.title}}</span>
-        <p>日時：{{detail.date}} {{detail.startTime}}〜{{detail.endTime}}</p>
-        <p>場所：{{detail.place}}</p>
-        <p>コメント：{{detail.comment}}</p>
+        <span class="card-title">{{schedule.title}}</span>
+        <p>日時：{{schedule.date}} {{schedule.startTime}}〜{{schedule.endTime}}</p>
+        <p>場所：{{schedule.place}}</p>
+        <p>コメント：{{schedule.comment}}</p>
       </div>
     </div>
-  
+
     <!--まとめ数字-->
     <div class="row section card">
       <div class="col s4 l4 center">
-        <h4>参加</h4>
-        <h4>1</h4>
+        <h4 class="blue-text">参加</h4>
+        <h4>{{OK}}</h4>
       </div>
       <div class="col s4 l4 center">
-        <h4>欠席</h4>
-        <h4>2</h4>
+        <h4 class="red-text">欠席</h4>
+        <h4>{{NG}}</h4>
       </div>
       <div class="col s4 l4 center">
-        <h4>未定</h4>
-        <h4>3</h4>
+        <h4 class="orange-text">未定</h4>
+        <h4>{{TBD}}</h4>
       </div>
     </div>
-  
+
     <!--リスト-->
     <div class="row">
-      <ul class="collection card" v-if="entryMember">
-        <li class="collection-item avatar" v-for="member in entryMember" v-bind:key="member['key']">
-          <!--<img v-if="item.image"
-                          :src="item.image"
-                          alt=""
-                          class="circle">-->
+      <ul class="collection card" v-if="members">
+        <li class="collection-item avatar" v-for="member in members" v-bind:key="member['.key']">
           <i class="material-icons circle ">perm_identity</i>
           <span class="title">{{member.name}}</span>
-          <p>{{member.entryComment}}</p>
+          <p>{{member.entry}} {{member.entryComment}}</p>
           <div class="secondary-content">
-            <a class="waves-effect waves-light btn modal-trigger" href="#modal1" @click="setModalMember(member)">出欠登録</a>
-  
+            <a v-if="member.entry=='OK'" class="waves-effect waves-light btn-flat blue white-text modal-trigger" href="#modal1" @click="modalMember = member">参加</a>
+            <a v-else-if="member.entry==='NG'" class="waves-effect waves-light btn-flat red white-text modal-trigger" href="#modal1" @click="modalMember = member">欠席</a>
+            <a v-else-if="member.entry==='TBD'" class="waves-effect waves-light btn-flat orange-text modal-trigger" href="#modal1" @click="modalMember = member">未定</a>
+            <a v-else class="waves-effect waves-light btn-flat blue-text modal-trigger" href="#modal1" @click="modalMember = member">回答</a>
           </div>
         </li>
       </ul>
     </div>
-  
+
     <!-- Modal Structure -->
     <div id="modal1" class="modal modal-fixed-footer">
       <div class="modal-content">
         <h4>出欠登録</h4>
-        <p>{{detail.title}} in {{detail.place}}</p>
-        <p>{{detail.date}} {{detail.startTime}}〜{{detail.endTime}}</p>
+        {{entry}} {{entryComment}}
+        <p>{{schedule.title}} in {{schedule.place}}</p>
+        <p>{{schedule.date}} {{schedule.startTime}}〜{{schedule.endTime}}</p>
         <p></p>
         <h4>{{modalMember.name}}の出欠</h4>
         <div class="row">
           <div class="col s4">
-            <button class="btn-large waves-effect waves-light teal">
+            <button @click="entry = 'OK'" class="btn-large waves-effect waves-light teal">
               <i class="material-icons left">panorama_fish_eye</i>
               参加
             </button>
           </div>
           <div class="col s4">
-            <button class="btn-large waves-effect waves-light teal lighten-5 black-text">
+            <button @click="entry = 'TBD'" class="btn-large waves-effect waves-light teal lighten-5 black-text">
               <i class="material-icons left">report_problem</i>
               未定
             </button>
           </div>
           <div class="col s4">
-            <button class="btn-large waves-effect waves-light teal lighten-5  black-text">
+            <button @click="entry = 'NG'" class="btn-large waves-effect waves-light teal lighten-5  black-text">
               <i class="material-icons left">close</i>
               欠席</button>
           </div>
         </div>
         <div class="input-field col s12">
           <i class="material-icons prefix">mode_edit</i>
-          <input v-model="comment" id="comment" type="text" class="validate">
+          <input v-model="entryComment" id="comment" type="text" class="validate">
           <label for="comment">コメント</label>
         </div>
       </div>
       <div class="modal-footer">
         <button @click="modalClose" class="modal-action modal-close btn-flat waves-effect waves-light">閉じる</button>
-        <button @click="modalClose" class="modal-action modal-close btn pink waves-effect waves-light">登録</button>
+        <button @click="sendEntry" class="modal-action modal-close btn pink waves-effect waves-light">登録</button>
       </div>
     </div>
-  
+
     <!--戻るボタン-->
     <button @click="goScheduleTop" class="btn waves-effect waves-light" type="submit" name="action">
       一覧に戻る
@@ -101,9 +100,14 @@ export default {
   data() {
     return {
       title: 'スケジュール詳細',
-      detail: {},
-      entryMember: [],
-      modalMember: {}
+      schedule: {},
+      members: [],
+      modalMember: {},
+      entry: '',
+      entryComment: '',
+      OK: 0,
+      NG: 0,
+      TBD: 0,
     }
   },
   created() {
@@ -111,14 +115,34 @@ export default {
   },
   firebase() {
     return {
-      detail: {
+      schedule: {
         source: db.ref("schedule").child(this.$route.params.id),
         asObject: true,
-        readyCallback: function (data) {
-          // entryテーブルのメンバー出欠情報を取ってくる
-          db.ref('entry/' + data.key).once('value')
-            .then(dataSnapshot => dataSnapshot.val())
-            .then(entry => this.entryMember = entry)
+        readyCallback: function(data) {
+        }
+      },
+      members: {
+        source: db.ref("entry").child(this.$route.params.id),
+        readyCallback: function(snapShot) {
+          if (!snapShot.val()) return;
+
+          const members = snapShot.val();
+          for (let k of Object.keys(members)) {
+            // console.log(members[k].entry);
+            switch (members[k].entry) {
+              case 'OK':
+                this.OK++
+                break;
+              case 'NG':
+                this.NG++
+                break;
+              case 'TBD':
+                this.TBD++
+                break;
+              default:
+                break;
+            }
+          }
 
           this.$Progress.finish()
         }
@@ -126,24 +150,41 @@ export default {
     }
   },
   mounted() {
-    $(document).ready(function () {
+    $(document).ready(function() {
       $('.modal').modal();
     });
   },
   methods: {
 
-    goScheduleTop: function () {
+    goScheduleTop: function() {
       this.$router.push('/schedule');
     },
-    goEdit: function () {
+    goEdit: function() {
       this.$router.push('/schedule/New');
     },
-    setModalMember(member) {
-      this.modalMember = member;
-    },
-    modalClose: function () {
+    modalClose: function() {
       $('#modal1').modal('close');
-    }
+      this.entry = ''
+      this.entryComment = '';
+    },
+    sendEntry: function() {
+      // 登録準備
+      const sendData = {
+        name: this.modalMember.name,
+        image: this.modalMember.image,
+        entry: this.entry,
+        entryComment: this.entryComment
+      }
+
+      // 登録
+      db.ref("entry").child(this.$route.params.id).child(this.modalMember['.key']).set(sendData)
+
+      // 初期化
+      this.modalMember = {};
+      this.entry = ''
+      this.entryComment = '';
+      $('#modal1').modal('close');
+    },
   }
 }
 </script>
